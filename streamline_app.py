7793acc11bd936ec5bee3c7a414eec2edbaf9abe
DamainQ <111399225+DamainQ@users.noreply.github.com>
2022-08-17 12:26:16 +1200

    Update streamline_app.py
    changed icon

--- streamline_app.py
+++ streamline_app.py
@@ -4,7 +4,7 @@ streamlit.title("My Parents New Healthy Dinner")
 
 streamlit.header('Breakfast Favourites')
 
-streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 
