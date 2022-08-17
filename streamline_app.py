852b6d3262756cbc1a802aef8d1465b8091d292d
DamainQ <111399225+DamainQ@users.noreply.github.com>
2022-08-18 10:05:20 +1200

    Update streamline_app.py
    fruit_choice

--- streamline_app.py
+++ streamline_app.py
@@ -28,8 +28,13 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+#asks the user for their fruit choice
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #makes the json format usable 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
